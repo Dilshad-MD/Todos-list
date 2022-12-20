cb32d0e1b87a3e7f5ca932d297b98ec29d7a3f2b
dilshad <mddilshad56056@gmail.com>
2022-12-20 11:15:56 +0530

    todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Todos-list';
+  list: any[] = [];
+
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, name: item });
+    console.log(this.list);
+  }
+  removeTask(id: number) {
+    console.log(id);
+    this.list = this.list.filter((item) => item.id !== id);
+  }
 }
